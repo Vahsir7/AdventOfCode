6f74417d2b4e5999ae4d2a070fbf4233caded266
Rishav Bairagya <rishavbairagya@gmail.com>
2024-12-05 21:51:21 +0000

    day 2 code

--- 2024/day2/problem1.py
+++ 2024/day2/problem1.py
@@ -4,12 +4,12 @@ with open ('input.txt', 'r') as file:
     s=0
     for i in range(l):
         report = list(map(int,data[i].split(' ')))
-        print (report )
+        #print (report )
         
         a = report[0]
         i = report [1]
         flag = -1
-        print(a,i,flag)
+        #print(a,i,flag)
         if i>a and i<a+3:
             flag = 1
         elif i<a and a<i+3:
@@ -18,13 +18,13 @@ with open ('input.txt', 'r') as file:
             flag = -1
         a=i
         for i in report[2:]:
-            print(a,i,flag)
+            #print(a,i,flag)
             if flag==-1:
                 break
             else:
-                if flag == 1 and( i<=a or i>=a+3):
+                if flag == 1 and( i-a not in [1,2,3]):
                     flag = -1
-                elif flag == 2 and( i>=a or a>=i+3):
+                elif flag == 2 and(a-i not in [1,2,3]):
                     flag=-1
             a=i
         if flag != -1:
